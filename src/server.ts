b6b3c1ce26ed1ead1b72dac591042bb5eef6877a
André Loureiro <65689062+Loureiro12@users.noreply.github.com>
2022-06-16 19:16:53 -0300

    feat: config Eslint and Prettier

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,6 @@ import express from "express";
 
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.json({ message: "Deu!" });
-});
+app.get("/", (request, response) => response.json({ message: "Deu!" }));
 
 app.listen(3333, () => console.log("Server is running!"));
